5631f48825def104fec7f0ce93b4462b73c7a421
Zack McBride <zmcbride@shopkeep.com>
2014-08-22 20:43:03 +0100

    variable names

--- Zapp/ViewController.swift
+++ Zapp/ViewController.swift
@@ -10,12 +10,12 @@ class ViewController: UIViewController {
         view1.backgroundColor = UIColor.blueColor()
         self.view.addSubview(view1)
 
-        var view2: UIView = UIView(frame: CGRectMake(50, 50, 100, 100))
-        view2.accessibilityLabel = "Cunt"
-        view2.backgroundColor = UIColor.blueColor()
-        view1.addSubview(view2)
+        var nestedView: UIView = UIView(frame: CGRectMake(50, 50, 100, 100))
+        nestedView.accessibilityLabel = "Cunt"
+        nestedView.backgroundColor = UIColor.blueColor()
+        view1.addSubview(nestedView)
 
-        // This doesn't
+        // This doesn't find the view
         var uncaughtView = Expect.viewWithAccessibilityLabel("CDC")
         println("'CDC' Not found, so we get nil: \(uncaughtView?)")
 
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         var caughtView = Expect.viewWithAccessibilityLabel("Hello")
         println("'Hello' found, so we get the view: \(caughtView?) with accessibilityLabel: \(caughtView?.accessibilityLabel)")
 
-        // This finds the view
+        // This also finds the view
         var caughtNestedView = Expect.viewWithAccessibilityLabel("Cunt")
         println("'Cunt' found, so we get the view: \(caughtNestedView?) with accessibilityLabel: \(caughtNestedView?.accessibilityLabel)")
     }
